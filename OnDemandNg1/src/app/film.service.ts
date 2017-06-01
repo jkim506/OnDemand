a782e39684c6a93a5f9e6ac7a44e4a18cb630778
AUCKLAND\Mustafa.Shehadeh <Mustafa.Shehadeh@vista.co>
2017-06-02 10:40:41 +1200

    add star wars hardcoded data

--- OnDemandNg1/src/app/film.service.ts
+++ OnDemandNg1/src/app/film.service.ts
@@ -9,6 +9,24 @@ export class FilmService {
       year: 2005,
       id: 1,
       image: '/assets/films/ff.jpg'
+    },
+    {
+      title: 'Star Wars: Episode IV - A New Hope',
+      year: '1977',
+      id: '10',
+      image: '/assets/films/a_new_hope.jpg'
+    },
+      {
+      title: 'Star Wars: Episode V - The Empire Strikes Back',
+      year: '1980',
+      id: '11',
+      image: '/assets/films/strikes_back.jpg'
+    },
+    {
+      title: 'Star Wars: Episode V - The Empire Strikes Back',
+      year: '1980',
+      id: '12',
+      image: '/assets/films/returnofthejedi.jpg'
     }
   ];
 

1806e19a7288775daef96da991520544fa0c7cf7
kerry.archibald <Kerry.Archibald@vista.co>
2017-06-02 10:26:55 +1200

    film 1

--- OnDemandNg1/src/app/film.service.ts
+++ OnDemandNg1/src/app/film.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FilmService {
 
+  public films = [
+    {
+      title: 'Fast & Furious',
+      year: 2005,
+      id: 1,
+      image: '/assets/films/ff.jpg'
+    }
+  ];
+
   constructor() { }
 
 }

a5113a5b8971f5b611ece79c5207f13a8490e5ac
Jay Kim <jkim506@aucklanduni.ac.nz>
2017-06-02 10:56:42 +1200

    added films too

--- OnDemandNg1/src/app/film.service.ts
+++ OnDemandNg1/src/app/film.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+
+//Easily the best data service in the world
 @Injectable()
 export class FilmService {
 
@@ -27,7 +29,31 @@ export class FilmService {
       year: '1980',
       id: '12',
       image: '/assets/films/returnofthejedi.jpg'
-    }
+    },
+    {
+      title: 'Titanic',
+      year: '1997',
+      id: '30',
+      image: '/assets/films/titanic.jpg'
+    },
+    {
+      title: 'The Lion King',
+      year: '1994',
+      id: '31',
+      image: '/assets/films/lion_king.jpg'
+    },
+    {
+      title: 'Spirited Away',
+      year: '2001',
+      id: '32',
+      image: '/assets/films/spirited_away.jpg'
+    },
+    {
+      title: 'Home Alone',
+      year: '1990',
+      id: '33',
+      image: '/assets/films/home_alone.jpg'
+    },
   ];
 
   constructor() { }

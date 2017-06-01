0b41e37ab9442ad1cad814f9c513c33dd9793474
AUCKLAND\Mustafa.Shehadeh <Mustafa.Shehadeh@vista.co>
2017-06-02 10:59:16 +1200

    add model for film && add get films method

--- OnDemandNg1/src/app/film.service.ts
+++ OnDemandNg1/src/app/film.service.ts
@@ -1,9 +1,10 @@
+import { Film } from './models/film-model';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FilmService {
 
-  public films = [
+  public films: Film[] = [
     {
       title: 'Fast & Furious',
       year: 2005,
@@ -12,42 +13,45 @@ export class FilmService {
     },
     {
       title: 'Star Wars: Episode IV - A New Hope',
-      year: '1977',
-      id: '10',
+      year: 1977,
+      id: 2,
       image: '/assets/films/a_new_hope.jpg'
     },
       {
       title: 'Star Wars: Episode V - The Empire Strikes Back',
-      year: '1980',
-      id: '11',
+      year: 1980,
+      id: 3,
       image: '/assets/films/strikes_back.jpg'
     },
     {
       title: 'Star Wars: Episode V - The Empire Strikes Back',
-      year: '1980',
-      id: '12',
+      year: 1980,
+      id: 4,
       image: '/assets/films/returnofthejedi.jpg'
     },
     {
       title: 'The Little Mermaid',
-      year: '1989',
-      id: '13',
+      year: 1989,
+      id: 5,
       image: '/assets/films/mermaid.jpg'
     },
     {
       title: 'Forrest Gump ',
-      year: '1994',
-      id: '14',
+      year: 1994,
+      id: 6,
       image: '/assets/films/forrestGump.jpg'
     },
     {
       title: 'Your Name',
-      year: '2016',
-      id: '15',
+      year: 2016,
+      id: 7,
       image: '/assets/films/yourname.jpg'
     }
   ];
 
   constructor() { }
 
+  getFilms(): Film[] {
+    return this.films;
+  }
 }

13a578fb1252194844f04d870525f2b3580c09bc
Jay Kim <jkim506@aucklanduni.ac.nz>
2017-06-02 10:41:20 +1200

    delete stuff + film calls

---
+++ Vista.OnDemand.Api/Vista.OnDemand.Api/Controllers/BookingCreationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using Vista.OnDemand.Api.Models;
+
+namespace Vista.OnDemand.Api.Controllers
+{
+    [RoutePrefix("api/booking")]
+    public class BookingCreationController : ApiController
+    {
+
+        [Route("")]
+        [HttpPost]
+        public void CreateGroupSalesPrivateBooking()
+        {
+
+        }
+    }
+}
---
+++ Vista.OnDemand.Api/Vista.OnDemand.Api/Controllers/FilmController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using Vista.OnDemand.Api.Models;
+
+namespace Vista.OnDemand.Api.Controllers
+{
+    [RoutePrefix("api/film")]
+    public class FilmController : ApiController
+    {
+        //nasty
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Film> GetFilms()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://qa.moviexchange.com/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = client.GetAsync("api/v1/releases").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var films = response.Content.ReadAsAsync<IEnumerable<Film>>().Result
+                                  .GroupBy(p => p.Title)
+                                  .Select(g => g.First())
+                                  .ToList();
+                return films;
+            }
+            else
+            {
+                throw new Exception();
+
+            }
+        }
+    }
+}

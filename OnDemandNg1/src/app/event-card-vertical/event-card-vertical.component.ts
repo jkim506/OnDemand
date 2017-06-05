02fc74c34dc0f823ea4b10e8222550e368418137
AUCKLAND\John.Lee <john.lee@vista.co>
2017-06-06 10:44:59 +1200

    Add vertical event card animations

--- OnDemandNg1/src/app/event-card-vertical/event-card-vertical.component.ts
+++ OnDemandNg1/src/app/event-card-vertical/event-card-vertical.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Event } from '../models/event.model';
 import { Router } from "@angular/router";
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-event-card-vertical',
   templateUrl: './event-card-vertical.component.html',
-  styleUrls: ['./event-card-vertical.component.scss']
+  styleUrls: ['./event-card-vertical.component.scss'],
+  animations: [
+    trigger('fadeInUp', [
+      state('in', style({ 'transform': 'translateY(0)', opacity: 1 })),
+      transition(':enter', [
+        style({ 'transform': 'translateY(20%)', opacity: 0 }),
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class EventCardVerticalComponent implements OnInit {
-
   @Input() event: Event;
   private percentage: number;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
   }
 
   goToEventDetails(){

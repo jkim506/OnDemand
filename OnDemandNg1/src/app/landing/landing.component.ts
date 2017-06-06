f4172249bdd2a0f0aaee2a9094381a4804740d84
AUCKLAND\John.Lee <john.lee@vista.co>
2017-06-06 13:03:28 +1200

    Trigger vertical event card animations one at a time

--- OnDemandNg1/src/app/landing/landing.component.ts
+++ OnDemandNg1/src/app/landing/landing.component.ts
@@ -1,16 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ViewContainerRef,
+  ComponentRef,
+  ComponentFactoryResolver
+} from '@angular/core';
 import { EventService } from '../event.service';
+import {EventCardVerticalComponent} from '../event-card-vertical/event-card-vertical.component';
 
 @Component({
   selector: 'app-landing',
+  entryComponents: [EventCardVerticalComponent],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, AfterViewInit {
+  @ViewChild('verticalEventCardsContainer', { read: ViewContainerRef })
+  private verticalEventCardsContainer: ViewContainerRef;
+  private verticalEventCards: ComponentRef<EventCardVerticalComponent>[] = [];
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private resolver: ComponentFactoryResolver) { }
 
   ngOnInit() {
+    let events = this.eventService.getEvents();
+    let $this = this;
+    events.forEach(function(event) {
+      console.log(event);
+      $this.addVerticalEventCard(event);
+    });
+  }
+
+  private addVerticalEventCard(event) {
+    let factory = this.resolver.resolveComponentFactory(EventCardVerticalComponent);
+    let newCard = this.verticalEventCardsContainer.createComponent(factory);
+    (<EventCardVerticalComponent>newCard.instance).event = event;
+    this.verticalEventCards.push(newCard);
+  }
+
+  private showCard(cardIndex) {
+    this.verticalEventCards[cardIndex].instance.isVisible = true;
+  }
+
+  ngAfterViewInit() {
+    let i = 0;
+    let interval = setInterval(() => {
+      if (i > this.verticalEventCards.length ) {
+        clearInterval(interval);
+      }
+      else {
+        this.showCard(i);
+        i++;
+      }
+    }, 200);
   }
 
 }
